86d9d636219614d726acf5e90c0368f421c9b012
rfilimon <rebeca.filimon@insbaixcamp.cat>
2025-04-02 13:53:11 +0200

    Afegim 3a linia

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Afegim una segona linia");
+        System.out.println("Afegim una tercera linia");
     }
 }
