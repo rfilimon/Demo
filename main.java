253124275571b854d8d36527f471a4acfa2ea915
rfilimon <rebeca.filimon@insbaixcamp.cat>
2025-04-04 14:04:16 +0200

    Afegeixo linia a Visual Studio Code

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class main {
         System.out.println("Hello world");
         System.out.println("Afegim una segona linia");
         System.out.println("Afegim una tercera linia");
+        System.out.println("Afegim una linia des de Visual Studio Code");
     }
 }
